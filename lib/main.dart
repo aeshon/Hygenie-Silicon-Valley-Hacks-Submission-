4a8e601fb67807f22e8ccbbc58568d27de656af1
Shreyas Rana <ranashreyas@gmail.com>
2020-04-22 14:52:05 -0700

    random change

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MapSampleState extends State<MapSample> {
   }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(37.42796133580664, -122.0857496559620),
     zoom: 14.4746,
   );
 
