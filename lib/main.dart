620fd9decd0f4fa66d5067e88c1c5a68f02e07ae
aeshon <aeshon.balasubramanian@gmail.com>
2020-04-22 14:34:40 -0700

    blah

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MapSampleState extends State<MapSample> {
       body: Column(
         children: <Widget>[
           Container(
-            height: 700,
+            height: 500,
             child: GoogleMap(
               mapType: MapType.hybrid,
               initialCameraPosition: _kGooglePlex,
