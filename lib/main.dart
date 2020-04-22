9c6c35a9acc34011a3aaaae09f25b8fe231b3386
Shreyas Rana <ranashreyas@gmail.com>
2020-04-22 12:02:00 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+  LocationData currentLocation;
+  Location location;
+
+  void initState() {
+    super.initState();
+    Fluttertoast.showToast(
+        msg: "changed location",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+    location = new Location();
+
+    location.onLocationChanged().listen((LocationData cLoc) {
+      // cLoc contains the lat and long of the
+      // current user's position in real time,
+      // so we're holding on to it
+      setState(() {
+        currentLocation = cLoc;
+      });
+    });
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 700,
+            child: GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+          (currentLocation == null)
+              ? Text("nothing yet")
+              : Text("Current Lat: " +
+              currentLocation.latitude.toString() +
+              "      Current Lon: " +
+              currentLocation.longitude.toString())
+        ],
+      ),
+
+//      floatingActionButton: FloatingActionButton.extended(
+//        onPressed: _goToTheLake,
+//        label: Text('To the lake!'),
+//        icon: Icon(Icons.directions_boat),
+//      ),
+    );
+  }
+
+//  static final CameraPosition _kLake = CameraPosition(
+//      bearing: 192.8334901395799,
+//      target: LatLng(37.43296265331129, -122.08832357078792),
+//      tilt: 59.440717697143555,
+//      zoom: 19.151926040649414);
+
+//  Future<void> _goToTheLake() async {
+//    final GoogleMapController controller = await _controller.future;
+//    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+//  }
+}
