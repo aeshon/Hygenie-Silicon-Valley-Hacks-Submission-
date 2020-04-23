f2e9813f04c2d8bf90284f68bc3f241217f645d2
arnavbhola <48901144+arnavbhola@users.noreply.github.com>
2020-04-23 12:07:13 -0700

    Added the basic PPE page with links to make ur own masks and sanitizer + link to gloves on amazon

---
+++ lib/covidessentials.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class FindPPE extends StatelessWidget {
+  Future launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: true, forceWebView: true);
+    } else {
+      print("Can't launch url $url");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: new Center(
+              child: new Text('COVID Essentials', textAlign: TextAlign.center)),
+        ),
+        body: Center(
+            child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            Text('Masks',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            RaisedButton(
+              child: Text(
+                  'Make your own mask! \n'
+                  'Click to learn more!',
+                  textAlign: TextAlign.center),
+              onPressed: () {
+                launchURL(
+                    "https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/cloth-face-cover.html");
+              },
+            ),
+            Spacer(),
+            Text('Gloves',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            RaisedButton(
+              child: Text('Find some on Amazon!'),
+              onPressed: () {
+                launchURL(
+                    "https://www.amazon.com/s?k=gloves+health&rh=n%3A3753541&ref=nb_sb_noss");
+              },
+            ),
+            Spacer(),
+            Text('Hand Sanitizer',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            RaisedButton(
+              child: Text(
+                  'Make your own hand sanitizer! \n Click to learn more!',
+                  textAlign: TextAlign.center),
+              onPressed: () {
+                launchURL(
+                    "https://www.healthline.com/health/how-to-make-hand-sanitizer");
+              },
+            ),
+            Spacer(),
+          ],
+        )),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,93 @@
-import 'dart:async';
-
+import 'package:covidnotifassistant/covidessentials.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-void main() => runApp(MyApp());
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:location/location.dart';
+//import 'package:fluttertoast/fluttertoast.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MapSample(),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    home: new FindPPE(),
+  ));
 }
 
-class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-  LocationData currentLocation;
-  Location location;
-
-  void initState() {
-    super.initState();
-    Fluttertoast.showToast(
-        msg: "changed location",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0);
-    location = new Location();
-
-    location.onLocationChanged().listen((LocationData cLoc) {
-      // cLoc contains the lat and long of the
-      // current user's position in real time,
-      // so we're holding on to it
-      setState(() {
-        currentLocation = cLoc;
-      });
-    });
-  }
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 500,
-            child: GoogleMap(
-              mapType: MapType.hybrid,
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            ),
-          ),
-          (currentLocation == null)
-              ? Text("nothing")
-              : Text("Current Lat: " +
-                  currentLocation.latitude.toString() +
-                  "      Current Lon: " +
-                  currentLocation.longitude.toString())
-        ],
-      ),
-
-//      floatingActionButton: FloatingActionButton.extended(
-//        onPressed: _goToTheLake,
-//        label: Text('To the lake!'),
-//        icon: Icon(Icons.directions_boat),
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter Google Maps Demo',
+//      home: MapSample(),
+//    );
+//  }
+//}
+//
+//class MapSample extends StatefulWidget {
+//  @override
+//  State<MapSample> createState() => MapSampleState();
+//}
+//
+//class MapSampleState extends State<MapSample> {
+//  Completer<GoogleMapController> _controller = Completer();
+//  LocationData currentLocation;
+//  Location location;
+//
+//  void initState() {
+//    super.initState();
+//    Fluttertoast.showToast(
+//        msg: "changed location",
+//        toastLength: Toast.LENGTH_SHORT,
+//        gravity: ToastGravity.CENTER,
+//        backgroundColor: Colors.red,
+//        textColor: Colors.white,
+//        fontSize: 16.0);
+//    location = new Location();
+//
+//    location.onLocationChanged().listen((LocationData cLoc) {
+//      // cLoc contains the lat and long of the
+//      // current user's position in real time,
+//      // so we're holding on to it
+//      setState(() {
+//        currentLocation = cLoc;
+//      });
+//    });
+//  }
+//
+//  static final CameraPosition _kGooglePlex = CameraPosition(
+//    target: LatLng(37.42796133580664, -122.085749655962),
+//    zoom: 14.4746,
+//  );
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      body: Column(
+//        children: <Widget>[
+//          Container(
+//            height: 500,
+//            child: GoogleMap(
+//              mapType: MapType.hybrid,
+//              initialCameraPosition: _kGooglePlex,
+//              onMapCreated: (GoogleMapController controller) {
+//                _controller.complete(controller);
+//              },
+//            ),
+//          ),
+//          (currentLocation == null)
+//              ? Text("nothing")
+//              : Text("Current Lat: " +
+//                  currentLocation.latitude.toString() +
+//                  "      Current Lon: " +
+//                  currentLocation.longitude.toString())
+//        ],
 //      ),
-    );
-  }
+//
+////      floatingActionButton: FloatingActionButton.extended(
+////        onPressed: _goToTheLake,
+////        label: Text('To the lake!'),
+////        icon: Icon(Icons.directions_boat),
+////      ),
+//    );
+//  }
 
 //  static final CameraPosition _kLake = CameraPosition(
 //      bearing: 192.8334901395799,
@@ -95,4 +99,4 @@ class MapSampleState extends State<MapSample> {
 //    final GoogleMapController controller = await _controller.future;
 //    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
 //  }
-}
+//}
