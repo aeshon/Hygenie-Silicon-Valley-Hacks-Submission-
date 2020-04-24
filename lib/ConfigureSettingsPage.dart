b29f7fd0390ece7b1b1ea81123ec9f70964e8729
aeshon <aeshon.balasubramanian@gmail.com>
2020-04-24 09:36:13 -0700

    working on UI

---
+++ lib/ConfigureSettingsPage.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ConfigureSettingsPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
