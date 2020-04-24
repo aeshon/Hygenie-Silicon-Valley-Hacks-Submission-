d4498269e700bcb37bacb6af0a8e549bc00d9f18
arnavbhola <48901144+arnavbhola@users.noreply.github.com>
2020-04-24 11:35:30 -0700

    Fixed location page. UI is finished. Location code needs to be added.

--- lib/useraddress.dart
+++ lib/useraddress.dart
@@ -1,74 +1,56 @@
 import 'package:flutter/material.dart';
 
-class UserAddress extends StatefulWidget {
-  UserAddress({Key key}) : super(key: key);
-
-  @override
-  UserAddressState createState() => new UserAddressState();
-}
-
-class UserAddressState extends State<UserAddress> {
-  final formKey = GlobalKey<FormState>();
-  String userAddress = "";
+class UserAddress extends StatelessWidget {
+  //UserAddress({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
+    return MaterialApp(
+      title: 'SnackBar Demo',
+      home: Scaffold(
+        appBar: AppBar(
           title: new Center(
               child: new Text('Location', textAlign: TextAlign.center)),
         ),
-        body: Card(
-          child: new Column(
-            children: <Widget>[
-              Spacer(
-                flex: 2,
-              ),
-              Text("Please enter your address to configure location settings",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue)),
-              Spacer(),
-              Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Address:'),
-                      onSaved: (input) => userAddress = input,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            onPressed: () {
-                              printAddress();
-                            },
-                            child: Text('Submit'),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Spacer(
-                flex: 2,
-              ),
-            ],
-          ),
-        ));
+        body: UserAddressState(),
+      ),
+    );
   }
+}
 
-  void printAddress() {
-    if (formKey.currentState.validate()) {
-      formKey.currentState.save();
-      print(userAddress);
-    }
+class UserAddressState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: new Column(
+        children: <Widget>[
+          Spacer(
+            flex: 3,
+          ),
+          Text("Please click 'Configure' to set your home address",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue)),
+          Spacer(),
+          RaisedButton(
+            onPressed: () {
+              //ENTER LOCATION CODE HERE
+
+              final snackBar = SnackBar(
+                content: Text('Location has been configured'),
+              );
+
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+            child: Text('Configure'),
+          ),
+          Spacer(
+            flex: 3,
+          ),
+        ],
+      ),
+    );
   }
 }
